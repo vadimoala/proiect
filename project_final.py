f507650dae04973603259eff1e3180e9cae0b1f9
vadimoala <vadimoala@gmail.com>
2024-06-26 17:48:30 +0300

    test

---
+++ project_final.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+
+    class Note:
+        def __init__(self, mari, mici, medii):
+            self.mari = mari
+            self.mici = mici
+            self.medii = medii
